637a795ae4f227019a4cd39ce1266203c128402a
Gleb Dmitriev <woodrunsdeep@gmail.com>
2020-09-14 23:07:42 +0300

    feat: add solution for simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,42 @@
-const CustomError = require("../extensions/custom-error");
-
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
+
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (value === undefined) {
+      this.chain.push('(  )');
+    } else {
+      this.chain.push(`( ${value} )`);
+    }
+    return this;
   },
+
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (position === undefined ||
+        position <= 0 ||
+        position > this.chain.length ||
+        typeof position !== 'number') {
+          this.chain = [];
+          throw new Error('Invalid position');
+        } else {
+          this.chain.splice(position-1, 1);
+          return this;
+        }
   },
+
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let result = this.chain;
+    this.chain = [];
+    return result.join('~~');
   }
-};
+}
 
 module.exports = chainMaker;
